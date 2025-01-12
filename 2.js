999e0f967224f92f86d761b6cb3061bcce8d914b
ox1s <linamel08v@gmail.com>
2025-01-12 19:45:37 +0300

    finished

--- 2.js
+++ 2.js
@@ -1,30 +1,35 @@
 function getAverage(scores) {
     let sum = 0;
-
+  
     for (const score of scores) {
-        sum += score;
+      sum += score;
     }
-
+  
     return sum / scores.length;
-}
-function getGrade(score) {
+  }
+  
+  function getGrade(score) {
     if (score === 100) {
-        return "A++";
-    }
-    else if (score >= 90) {
-        return "A";
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
     } else if (score >= 80) {
-        return "B";
+      return "B";
     } else if (score >= 70) {
-        return "C";
+      return "C";
     } else if (score >= 60) {
-        return "D";
+      return "D";
     } else {
-        return "F";
+      return "F";
     }
-}
-
-
-console.log(getGrade(96));
-console.log(getGrade(82));
-console.log(getGrade(56));
+  }
+  
+  function hasPassingGrade(score) {
+    return getGrade(score) !== "F";
+  }
+  
+  function studentMsg(totalScores, studentScore) {
+    return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(studentScore)}. You ${hasPassingGrade(studentScore) ? "passed" : "failed"} the course.`;
+  }
+  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+  
