ccd454be68175ce5375f4b9ec140aef3f65ca7aa
ox1s <linamel08v@gmail.com>
2025-01-14 14:22:18 +0300

    finish

--- RolePlayingGame/script.js
+++ RolePlayingGame/script.js
@@ -116,8 +116,11 @@ const attack = () => {
             defeatMonster();
         }
     }
-    if(Math.random() <= .1)
+    if (Math.random() <= .1) {
         text.innerText += ` Your ${inventory.pop()} breaks.`;
+        currentWeaponIndex--;
+    }
+
 };
 const dodge = () => {
     text.innerText = `You dodged the attack from the ${monsters[fighting].name}`;
@@ -163,6 +166,31 @@ const lose = () => {
 const winGame = () => {
     update(locations[6]);
 };
+const easterEgg = () => update([locations[7]]);
+const pick = (guess) => {
+    const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = `You picked ${guess}. Here are the random numbers:\n`;
+    for (let i = 0; i < 10; i++) {
+        text.innerText += `${numbers[i]}\n`;
+    }
+    if (numbers.includes(guess)) {
+        text.innerText += "Right! You win 20 gold!";
+        gold += 20;
+        goldText.innerText = gold;
+    }
+    else {
+        text.innerText += "Wrong! You lose 10 health!";
+        health -= 10;
+        healthText.innerText = health;
+        if (health <= 0)
+            lose();
+    }
+};
+const pickTwo = () => pick(2);
+const pickEight = () => pick(8);
 
 // initialize game arrays
 const locations = [
@@ -193,7 +221,7 @@ const locations = [
     {
         name: "kill monster",
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
+        "button functions": [goTown, easterEgg, goTown],
         text: "The monster screams \"Arg!\" as it dies. You gain experience points and find gold."
     },
     {
@@ -207,6 +235,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+    },
+    {
+        name: "easter egg",
+        "button text": ["2", "8", "Go to town square?"],
+        "button functions": [pickTwo, pickEight, goTown],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ];
 
@@ -226,3 +260,5 @@ const monsters = [
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+
