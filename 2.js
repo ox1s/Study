708df4cc36003896627e243b41293aa94c10d6b9
ox1s <linamel08v@gmail.com>
2025-01-14 13:58:19 +0300

    2

--- 2.js
+++ 2.js
@@ -1,35 +1,46 @@
 function getAverage(scores) {
-    let sum = 0;
-  
-    for (const score of scores) {
-      sum += score;
-    }
-  
-    return sum / scores.length;
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
   }
-  
-  function getGrade(score) {
-    if (score === 100) {
-      return "A++";
-    } else if (score >= 90) {
-      return "A";
-    } else if (score >= 80) {
-      return "B";
-    } else if (score >= 70) {
-      return "C";
-    } else if (score >= 60) {
-      return "D";
-    } else {
-      return "F";
-    }
+
+  return sum / scores.length;
+}
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
   }
-  
-  function hasPassingGrade(score) {
-    return getGrade(score) !== "F";
+}
+
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+
+function studentMsg(totalScores, studentScore) {
+  return `Class average: ${ Math.round(getAverage(totalScores) * 10) / 10}. Your grade: ${getGrade(studentScore)}. You ${hasPassingGrade(studentScore) ? "passed" : "failed"} the course.`;
+}
+
+function fillArrayWithRandomNumbers(length, min, max) {
+  const randomNumbers = [];
+
+  for (let i = 0; i < length; i++) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumbers.push(randomNumber);
   }
-  
-  function studentMsg(totalScores, studentScore) {
-    return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(studentScore)}. You ${hasPassingGrade(studentScore) ? "passed" : "failed"} the course.`;
-  }
-  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
-  
+
+  return randomNumbers;
+}
+console.log(studentMsg(fillArrayWithRandomNumbers(7,1,100), 73));
+
