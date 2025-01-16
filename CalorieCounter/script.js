8ef0e7f67f740afdac879e991558eebee06d8c37
ox1s <linamel08v@gmail.com>
2025-01-16 19:37:19 +0300

    calorie counter is finished

--- CalorieCounter/script.js
+++ CalorieCounter/script.js
@@ -27,11 +27,31 @@ const calculateCalories = (e) => {
     const dinnerNumberInputs = document.querySelectorAll(`#dinner input[type='number']`);
     const snacksNumberInputs = document.querySelectorAll(`#snacks input[type='number']`);
     const exerciseNumberInputs = document.querySelectorAll(`#exercise input[type='number']`);
+
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
     const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
+    if (isError) {
+        return;
+    }
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+
+    const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+
+
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+    `;
+    output.classList.remove('hide');
 };
 const getCaloriesFromInputs = (list) => {
     let calories = 0;
@@ -47,5 +67,15 @@ const getCaloriesFromInputs = (list) => {
     }
     return calories;
 };
+const clearForm = () => {
+    const inputContainers = Array.from(document.querySelectorAll(".input-container"));
+    for (const container of inputContainers) {
+        container.innerHTML = "";
+    }
+    budgetNumberInput.value = '';
+    output.innerText = '';
+    output.classList.add('hide');
+};
 addEntryButton.addEventListener("click", addEntry);
-
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm);
