d9ad851f042587fad4c57bc220cb9305c980e742
ox1s <linamel08v@gmail.com>
2025-08-22 14:40:40 +0300

    Чистка мусора

--- c-housework/problem_pf.c
+++ c-housework/problem_pf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <limits.h>
 /*
 Problem PF -- простые числа, подобные числам Фибоначчи
 
@@ -28,13 +29,116 @@ Seqi = k * Seqi - 1 + n * Seqi - 2
 
 Вполне может быть и так, что в последовательности нет простых чисел. Тогда вы должны вывести на стандартный вывод 0
 */
+unsigned long long mult_mod(unsigned long long x, unsigned long long y,
+                            unsigned long long m)
+{
+    unsigned long long res = 0;
+    x = x % m;
 
-int main() {
-    int n;
-    int res = scanf("%d", &n);
-    if (res != 1) {
+    while (y > 0) {
+        if (y % 2 == 1)
+            res = (res + x) % m;
+        x = (x * 2) % m;
+        y /= 2;
+    }
+    return res;
+}
+
+
+
+unsigned long long pow_mod(unsigned long long n, unsigned long long k,
+                           unsigned long long m)
+{
+    unsigned long long mult = n % m;
+    unsigned long long prod = 1;
+
+    while (k > 0) {
+        if (k % 2 == 1) {
+            prod = mult_mod(mult, prod, m);
+            k -= 1;
+        }
+        mult =  mult_mod(mult, mult, m);
+        k /= 2;
+    }
+
+    return prod;
+}
+
+unsigned long long gcd(unsigned long long x, unsigned long long y)
+{
+    while (y) {
+        x = x % y;
+        unsigned long long q = x;
+        x = y;
+        y = q;
+    }
+    return x;
+
+}
+
+int is_prime_small(unsigned long long n)
+{
+    if ((n == 2) || (n == 3))
+        return 1;
+    if ((n < 2) || ((n % 2) == 0) || ((n % 3) == 0))
+        return 0;
+
+    for (int j = 5; j * j <= n; j += 6)
+        if (((n % j) == 0) || ((n % (j + 2)) == 0))
+            return 0;
+
+    return 1;
+}
+
+int is_prime(unsigned long long p)
+{
+    if (p < 1000) {
+        int ans = is_prime_small(p);
+        return ans;
+    }
+    for (int i = 0; i < 100; ++i) {
+        unsigned long long a = rand() % p;
+        if ((gcd(a, p) != 1) || (pow_mod(a, p - 1, p) != (1 % p)))
+            return 0;
+    }
+    return 1;
+}
+
+
+unsigned long long find_max_prime(int k, int n)
+{
+    unsigned long long first = 0, second = 1;
+    unsigned long long found = 0;
+
+    while (1) {
+        unsigned long long tmp = second;
+        if (is_prime(second) == 1)
+            found = second;
+
+        second = n * first + k * second;
+        first = tmp;
+
+#if 0
+        printf("\n\tsecond = %llu\n\tfirst = %llu\n", second, first);
+        printf("\t\tres = %llu \n", n * first + k * second);
+#endif
+
+        if (second < first)
+            break;
+    }
+
+    return found;
+}
+
+int main()
+{
+    int k, n;
+    int res = scanf("%d%d", &k, &n);
+    if (res != 2) {
         printf("Wrong input!");
         abort();
     }
+    unsigned long long p = find_max_prime(k, n);
+    printf("%llu", p);
     return 0;
 }
